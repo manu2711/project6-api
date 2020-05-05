1f4da0438f0c1473b6ae892633b440ff165d3340
Emmanuel Delbar <edelbar.fr@gmail.com>
2020-05-05 11:55:19 +0200

    Creation of database and connection to API

--- app.js
+++ app.js
@@ -4,4 +4,21 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+mongoose
+  .connect('mongodb+srv://accessUser:5K7F9UgwbVvxcd3W@cluster0-ruufw.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connection to database successful !'))
+  .catch(() => console.log('Connection to database failed'))
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  next()
+})
+
+app.use(bodyParser.json())
+
 module.exports = app
